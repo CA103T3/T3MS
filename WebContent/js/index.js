f705124404e774bff0b9552b3bedb04f7df020bd
Han Huang <han0913@gmail.com>
2018-08-27 15:22:11 +0800

    add .slideanim .slide

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,10 +1,35 @@
-//http://www.dwuser.com/education/content/creating-a-jquery-image-scroller/
-$(function(){
+/**
+ * display close icon and close div
+ *
+ * @param  string div - div #id selector (e.g. #float-div-left)
+ * @param  string xitem - span .class selector (e.g. .close-item)
+ * @return void
+ */
+/*
+function close_div(div, xitem)
+{
+    // display close icon or not
+    $(div).mouseover(function() {
+        $(div + " " + xitem).css("display", "block");
+        //console.log("mouseover");
+    }).mouseout(function() {
+        $(div + " " + xitem).css("display", "none");
+        //console.log("mouseout");
+    });
+
+    // close div
+    $(div + " " + xitem).click(function() {
+        $(div).css("display", "none");
+    });
+}
+*/
+
+$(document).ready(function(){
     //for scroller
     $(window).on("resize", function () {
         $("body").css("padding-top", $("nav").height());
     });
-
+    //http://www.dwuser.com/education/content/creating-a-jquery-image-scroller/
     $("#scroller").prop("width", $(window).width());
     var scroller = $('#scroller div.innerScrollArea');
     var scrollerContent = scroller.children('ul');
@@ -17,20 +42,20 @@ $(function(){
     });
     var fullW = curX / 2;
     var viewportW = scroller.width();
-	
+    
     //First, we have a slightly non-traditional use of the jQuery .animate() method.  We use it to tween (animate) speed changes in our scroller, avoiding jerky stops and starts.
     // Scrolling speed management
     var controller = {curSpeed:0, fullSpeed:2}; //The fullSpeed:2 value can be changed to adjust the scrolling speed.
-	//console.log(controller);
+    //console.log(controller);
     var $controller = $(controller);
-	//console.log($controller);
-	//console.log($controller instanceof jQuery);
+    //console.log($controller);
+    //console.log($controller instanceof jQuery);
     var tweenToNewSpeed = function(newSpeed, duration)
     {
         if (duration === undefined)
             duration = 600; //This value can be changed to make the rotator stop and start more quickly or slowly.
         $controller.stop(true).animate({curSpeed:newSpeed}, duration);
-		//console.log("tweenToNewSpeed : controller.curSpeed - " + newSpeed);
+        //console.log("tweenToNewSpeed : controller.curSpeed - " + newSpeed);
     };
 
     //We use the .hover() method to listen for mouse interaction, pausing and resuming the scroller on hover and hover-out respectively.
@@ -49,12 +74,12 @@ $(function(){
     var doScroll = function()
     {
         var curX = scroller.scrollLeft();
-		//console.log("doScroll : controller.curSpeed - " + controller.curSpeed);
+        //console.log("doScroll : controller.curSpeed - " + controller.curSpeed);
         var newX = curX + controller.curSpeed;
         if (newX > fullW*2 - viewportW) //Whenever we reach the end, we jump back.
             newX -= fullW;
         scroller.scrollLeft(newX);  //We use the jQuery .scrollLeft() method to get and apply the scroll position.
-		//console.log(newX);
+        //console.log(newX);
     };
     //Finally, we use setInterval to call the scrolling method every 20 milliseconds.  We also set the scrolling speed to "full steam ahead."
     setInterval(doScroll, 20);
@@ -90,31 +115,16 @@ $(function(){
     
     //close_div("#announce",".close-item", tweenToNewSpeed, controller, scroller);
     close_div("#lightBox",".close-item", tweenToNewSpeed, controller, scroller);
-
+    
+    // Slide in elements on scroll
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+  
+        var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+      });
+    });
 });
-
-/**
- * display close icon and close div
- *
- * @param  string div - div #id selector (e.g. #float-div-left)
- * @param  string xitem - span .class selector (e.g. .close-item)
- * @return void
- */
-/*
-function close_div(div, xitem)
-{
-    // display close icon or not
-    $(div).mouseover(function() {
-        $(div + " " + xitem).css("display", "block");
-        //console.log("mouseover");
-    }).mouseout(function() {
-        $(div + " " + xitem).css("display", "none");
-        //console.log("mouseout");
-    });
-
-    // close div
-    $(div + " " + xitem).click(function() {
-        $(div).css("display", "none");
-    });
-}
-*/
